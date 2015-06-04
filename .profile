4453b4fc6ab2bede46af31737ce3e72766738fa8
Chris Bernal <thechrisbernal@gmail.com>
2015-06-03 18:23:15 -0700

    initial

---
+++ .profile
@@ -0,0 +1,9 @@
+alias bashedit="nano ~/.profile"
+alias reload="source ~/.profile"
+
+alias c="clear"
+alias del="rm -rf"
+alias l="ls -l"
+alias ll="ls -la"
+alias vi="nano"
+alias vim="nano"
---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+raspi-config
+
+adduser chris
+adduser chris sudo
+
+apt-get update -y && apt-get upgrade -y
+apt-get install samba samba-common-bin git screen avahi-daemon screen tmux curl ranger dialog htop boxes usbmount wicd-curses -y
+rpi-update
+
+nano /etc/samba/smb.conf
+
+smbpasswd –a root
+smbpasswd –a pi
+smbpasswd –a chris
+
+/etc/init.d/samba stop 
+/etc/init.d/samba start
+
+nano /etc/hosts
+nano /etc/hostname
+/etc/init.d/hostname.sh
+
+wicd-curses
+
+reboot
