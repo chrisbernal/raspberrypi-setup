8a1dbd0edf347f2f941a6deec0eb76af6f2a0a11
Chris Bernal <chrisbernal@users.noreply.github.com>
2015-06-04 17:54:59 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,28 +1,47 @@
 #!/bin/bash
+
+echo "please enter a username"
+read USER
+
+echo "running raspi-config"
 raspi-config
 
+echo "loading bash profile"
 . .profile
 
-adduser chris
-adduser chris sudo
+echo "adding new user"
+adduser $USER
 
+echo "adding new user to sudoers"
+adduser $USER sudo
+
+echo "updating"
 apt-get update -y && apt-get upgrade -y
+
+echo "installing utils"
 apt-get install samba samba-common-bin git screen avahi-daemon screen tmux curl ranger dialog htop boxes usbmount wicd-curses -y
+
+echo "updating rpi"
 rpi-update
 
+echo "edit line 250 of this file to 'no' instead of 'yes'"
 nano /etc/samba/smb.conf
 
+echo "adding users to samba share"
 smbpasswd –a root
 smbpasswd –a pi
-smbpasswd –a chris
+smbpasswd –a $USER
 
+echo "restarting samba"
 /etc/init.d/samba stop 
 /etc/init.d/samba start
 
-nano /etc/hosts
-nano /etc/hostname
-/etc/init.d/hostname.sh
-
+echo "setting up wifi"
+echo "---------------"
+echo "use the up and down arrow keys to navigate to your preferred network"
+echo "then use the right arrow to config, enter the password and enable auto connect"
+echo "then press q to quit"
 wicd-curses
 
+echo "rebooting"
 reboot
