6d45afd1383967503552461754558d50c45b99f1
Chris Bernal <thechrisbernal@gmail.com>
2015-06-17 19:51:58 -0700

    update

--- setup.sh
+++ setup.sh
@@ -3,39 +3,6 @@
 
 
 
-# YES NO FUNCTION
-function YESNO() {
-  echo "Run $1?"
-  select yn in "Yes" "No"
-  do
-  case $yn in
-      Yes ) $1;;
-      No ) echo "Skipped $1";;
-  esac
-  done
-  unset YESNO
-}
-
-
-
-function if_user_exists() {
-  UserExist()
-  {
-     awk -F":" '{ print $1 }' /etc/passwd | grep -x $1 > /dev/null
-     return $?
-  }
-  UserExist $1
-  if [ $? = 0 ]; then
-     sudo rm -rf /home/$1/.profile
-     sudo git clone https://gist.github.com/e022cdfada270a414418.git /home/$1/.profile
-  else
-    echo '$1 user account does not exist. skipping profile install.'
-  fi
-}
-
-
-
-
 # RUN FUNCTIONS
 function install_bash_profile() {
   if_user_exists chris
@@ -69,12 +36,43 @@ function reboot_now() {
 
 
 
-# initialize
-YESNO tplink_driver_install
-YESNO aptget_update_upgrade
-YESNO aptget_dist_upgrade
-YESNO aptget_install_apps
-YESNO adafrtuit_tft_isntall
-YESNO edit_network_iface
-YESNO reboot_now
+# IF USER EXISTS
+function if_user_exists() {
+  UserExist()
+  {
+     awk -F":" '{ print $1 }' /etc/passwd | grep -x $1 > /dev/null
+     return $?
+  }
+  UserExist $1
+  if [ $? = 0 ]; then
+     sudo rm -rf /home/$1/.profile
+     sudo git clone https://gist.github.com/e022cdfada270a414418.git /home/$1/.profile
+  else
+    echo '$1 user account does not exist. skipping profile install.'
+  fi
+}
+
+
+
+
+# RUN SETUP
+function RUN_SETUP() {
+  echo "Run Setup"
+  select setup_item in "TPLINK_DRIVER_INSTALL" "APTGET_UPDATE_UPGRADE" "APTGET_DIST_UPGRADE" "APTGET_INSTALL_APPS" "ADAFRTUIT_TFT_ISNTALL" "EDIT_NETWORK_IFACE" "REBOOT_NOW" "EXIT"
+  do
+  case $setup_item in
+      TPLINK_DRIVER_INSTALL )   tplink_driver_install;;
+      APTGET_UPDATE_UPGRADE )   aptget_update_upgrade;;
+      APTGET_DIST_UPGRADE )     aptget_dist_upgrade;;
+      APTGET_INSTALL_APPS )     aptget_install_apps;;
+      ADAFRTUIT_TFT_ISNTALL )   adafrtuit_tft_isntall;;
+      EDIT_NETWORK_IFACE )      edit_network_iface;;
+      REBOOT_NOW )              reboot_now;;
+      EXIT )                    exit;;
+  esac
+  done
+}
+
+
+RUN_SETUP
 echo 'setup finished'
